afcbd64e52899e49d2c5ab816d9c6ab5a5d910ac
jaspreet singh <js480292@gmail.com>
2025-12-14 17:17:06 +0530

    made a tweet app using django

---
+++ tweet/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for Pytweet project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('',view=views.tweet_list ,name='tweet_list'),
+    path('create/',view=views.create_tweet ,name='tweet_create'),
+    path('<int:tweet_id>/delete/',view=views.delete_tweet ,name='tweet_delete'),
+    path('<int:tweet_id>/edit/',view=views.edit_tweet ,name='tweet_edit'),
+    path('register/',view=views.register,name="register")
+
+]
